035b33fadf37d24d6e5fa9846c8766e3b9d11d38
abduljavedkhan <abduljaved314@gmail.com>
2021-08-17 16:42:36 +0530

    refactor chart

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,18 @@
 import React, { useState, useRef, useMemo } from "react";
 import img from "../../assets/images/g3.jpg";
-import { textColor } from "../../utils/constant";
 import Chart from "../Chart";
+import { textColor } from "../../utils/constant";
 
 const Card = ({ city, aqi, updatedAt, status }) => {
   const aqiData = useRef([]);
-  const [showModal, setShowModal] = useState(false);
+  const [showChart, setShowChart] = useState(false);
   aqiData.current = useMemo(() => [...aqiData.current, aqi], [aqi]);
 
   return (
     <>
       <div
         className="flex flex-col align-center justify-center flex-wrap m-2 py-3 h-24 w-72 shadow-md hover:shadow-xl rounded-lg border-2 border-blue-300 bg-white  cursor-pointer"
-        onClick={() => setShowModal(true)}
+        onClick={() => setShowChart(true)}
       >
         <div className="flex h-16 w-full my-1 align-center justify-center item-center">
           <div className=" w-1/5 border-2 border-blue-300 rounded-lg">
@@ -20,50 +20,33 @@ const Card = ({ city, aqi, updatedAt, status }) => {
               src={img}
               className="rounded-xl object-cover hover:opacity-70 p-1 h-full w-full overflow-hidden"
               alt="graph"
-              onClick={() => setShowModal(true)}
+              onClick={() => setShowChart(true)}
             />
           </div>
           <div className="flex flex-col w-3/5 h-16 justify-center align-center item-center mx-2 px-2 my-1 py-1 overflow-hidden">
             <div className="font-bold text-md">{city}</div>
             <div
-              className={`text-md font-semibold ${textColor(aqi)}`}
+              className={`text-md font-semibold ${useMemo(
+                () => textColor(aqi),
+                [aqi]
+              )}`}
             >{`${aqi}`}</div>
             <div className="text-gray-400 text-sm">{updatedAt}</div>
           </div>
         </div>
       </div>
-      {showModal ? (
+      {showChart ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-2 px-2">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-center justify-center m-4 p-2 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-md font-semibold text-blue-500 mx-2 px-2">
-                    {city}
-                  </h3>
-                  <span
-                    className={` font-semibold ${textColor(aqi)} text-md`}
-                  >{`${aqi}`}</span>
-                  <button
-                    className="text-red-500 bg-white border-2 border-red-500 rounded-md font-bold uppercase px-2 py-1 m-1 ml-16 mr-2 text-sm outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    x
-                  </button>
-                </div>
-                <Chart aqiData={aqiData.current} />
-                <div className="flex justify-center mx-3 px-2 my-1 py-1">
-                  <span className="text-sm text-gray-700 m-2 p-2">
-                    Status :{" "}
-                    <span className="text-green-600 mx-1 px-1">{`${status} `}</span>
-                    Last Updated:
-                    <span className="text-green-500 mx-1 px-1">
-                      {`${updatedAt}`}{" "}
-                    </span>
-                  </span>
-                </div>
-              </div>
+          <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none mx-4 px-2">
+            <div className="relative my-6 max-w-3xl w-full">
+              <Chart
+                onChartClick={() => setShowChart(false)}
+                status={status}
+                aqiData={aqiData.current}
+                city={city}
+                aqi={aqi}
+                updatedAt={updatedAt}
+              />
             </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
@@ -73,4 +56,4 @@ const Card = ({ city, aqi, updatedAt, status }) => {
   );
 };
 
-export default React.memo(Card);
+export default Card;
--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,13 +1,46 @@
 import React from "react";
 import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
+import { textColor } from "../../utils/constant";
 
-const Chart = ({aqiData}) => {
+const Chart = ({ aqiData, city, aqi, status, updatedAt, onChartClick }) => {
   return (
-    <div className="flex justify-center m-15 p-10 w-96 h-44 align-center item-center">
-      <Sparklines data={aqiData} limit={50}>
-        <SparklinesLine style={{ stroke: "#2991c8", fill: "none" }} />
-        <SparklinesSpots />
-      </Sparklines>
+    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+      <div className="flex items-center justify-between m-4 p-2">
+        <div className="flex text-sm  sm:text-sm  text-gray-600 mx-2 px-2 justify-between font-bold flex-wrap">
+          <div className=" mx-1 px-1">
+            City :{" "}
+            <span className="text-sm font-semibold text-blue-500">{`${city} `}</span>
+          </div>
+          <div className=" mx-1 px-1">
+            AQI :{" "}
+            <span className={`font-semibold ${textColor(aqi)} text-md`}>
+              {`${aqi}`}
+            </span>
+          </div>
+        </div>
+        <button
+          className="text-red-500 bg-white border-2 border-red-500 rounded-md font-bold uppercase px-2 py-1 m-1 ml-8 mr-4 pr-2 text-sm outline-none focus:outline-none mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={onChartClick}
+        >
+          x
+        </button>
+      </div>
+      <div className="flex justify-center m-5 p-10 w-auto h-60 align-center item-center  overflow-hidden">
+        <Sparklines data={aqiData} limit={50} over>
+          <SparklinesLine style={{ stroke: "#2991c8", fill: "none" }} />
+          <SparklinesSpots />
+        </Sparklines>
+      </div>
+      <div className="flex text-sm  sm:text-sm  text-gray-700 mx-2 px-2 justify-between font-bold flex-wrap m-1 p-2">
+        <div className=" mx-1 px-1">
+          Status : <span className="text-green-600">{`${status} `}</span>
+        </div>
+        <div className=" mx-1 px-1">
+          Last Updated :{" "}
+          <span className="text-green-500">{`${updatedAt}`} </span>
+        </div>
+      </div>
     </div>
   );
 };
